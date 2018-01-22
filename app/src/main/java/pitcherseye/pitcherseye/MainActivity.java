c12cf9d22c68e6c7d35d3104f00a366b31f836a6
connor-dean <connor.dean@washburn.edu>
2018-01-22 15:54:12 -0600

    Revert "Kind of a mess"
    This reverts commit ba46e0ef9b030d2ff9da4dbe93c8679caff8a473.

--- app/src/main/java/pitcherseye/pitcherseye/MainActivity.java
+++ app/src/main/java/pitcherseye/pitcherseye/MainActivity.java
@@ -1,32 +1,13 @@
 package pitcherseye.pitcherseye;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
-    IntentHelper aIntentHelper = new IntentHelper();
-
-    // Button mTestButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        changeIntent();
-    }
-
-    public void changeIntent() {
-        Button mTestButton = (Button) findViewById(R.id.test_button);
-        mTestButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                aIntentHelper.changeActivity(v, IntentHelper.class);
-            }
-        });
     }
 }
