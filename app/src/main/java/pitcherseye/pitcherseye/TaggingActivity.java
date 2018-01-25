7b00ea5a41d16aa6ac5cf9b5df2891e83bed695c
Connor Dean <connor.dean@washburn.edu>
2018-01-25 13:50:15 -0600

    Added UI workflows

---
+++ app/src/main/java/pitcherseye/pitcherseye/TaggingActivity.java
@@ -0,0 +1,28 @@
+package pitcherseye.pitcherseye;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class TaggingActivity extends AppCompatActivity {
+
+    // Buttons
+
+
+    // Request Code
+    int REQUEST_CODE_CALCULATE = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tagging);
+    }
+
+    public static Intent newIntent(Context packageContext) {
+        Intent i = new Intent(packageContext, TaggingActivity.class);
+        return i;
+    }
+
+}
