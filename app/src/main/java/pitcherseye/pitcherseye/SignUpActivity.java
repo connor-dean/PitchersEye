4f6fafbf942f76bf74e62f68ccfa3c76c90f61df
connor-dean <connor.dean@washburn.edu>
2018-01-28 00:47:43 -0600

    Authentication added, NEEDS refactoring

--- app/src/main/java/pitcherseye/pitcherseye/SignUpActivity.java
+++ app/src/main/java/pitcherseye/pitcherseye/SignUpActivity.java
@@ -5,25 +5,32 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUpActivity extends AppCompatActivity {
 
+    // UI Components
+    Button mRegisterButton;
     EditText mSignUpEmail;
     EditText mSignUpPassword;
+    FirebaseAuth mAuth;
+    ProgressBar mSignUpProgress;
 
-    Button mRegisterButton;
-
-    private FirebaseAuth mAuth;
+    // Request Code
+    int REQUEST_CODE_CALCULATE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +40,15 @@ public class SignUpActivity extends AppCompatActivity {
         // Instantiate Firebase object
         mAuth = FirebaseAuth.getInstance();
 
+        // Instantiate and hide progress bar
+        mSignUpProgress = (ProgressBar) findViewById(R.id.progress_signup);
+        mSignUpProgress.setVisibility(View.GONE);
+
+        // Register event
+        registerEvent();
+    }
+
+    private void registerEvent() {
         mRegisterButton = (Button) findViewById(R.id.button_signup_register);
         mRegisterButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,53 +58,61 @@ public class SignUpActivity extends AppCompatActivity {
         });
     }
 
-    // Method to register users, this is mostly copy-pasta, so take this with a grain
-    // of salt ...
+    // Receives input from text fields and verifies credentials
     private void registerUser() {
         mSignUpEmail = (EditText) findViewById(R.id.edt_signup_email);
         mSignUpPassword = (EditText) findViewById(R.id.edt_signup_password);
 
-        String email = mSignUpEmail.getText().toString().trim();
-        String password = mSignUpPassword.getText().toString().trim();
+        final String email = mSignUpEmail.getText().toString().trim();
+        final String password = mSignUpPassword.getText().toString().trim();
 
-        // Validate username
+        // Validate email, doesn't check valid emails, just the form for now
         if (email.isEmpty() || email == null) {
             mSignUpEmail.setError("Email is required.");
             mSignUpEmail.requestFocus();
+            return;
         } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
             mSignUpEmail.setError("You've entered an invalid email.");
             mSignUpEmail.requestFocus();
+            return;
         }
 
-        // Validate password
+        // Validate password, we'll probably want to require special characters/casing in the future
         if (password.isEmpty()  || password == null) {
             mSignUpPassword.setError("Password is required.");
             mSignUpPassword.requestFocus();
+            return;
         } else if (password.length() < 6) {
             mSignUpPassword.setError("Password requires at least 6 characters");
             mSignUpPassword.requestFocus();
-        } // Probably want to require special characters/casing in the future
+            return;
+        }
 
+        // Display the progress bar while loading
+        mSignUpProgress.setVisibility(View.VISIBLE);
+
+        // Return if the login was successful or invalid
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            // Log.d(TAG, "createUserWithEmail:success");
-                            // FirebaseUser user = mAuth.getCurrentUser();
-                            // updateUI(user);
-                            Toast.makeText(SignUpActivity.this, "Successful", Toast.LENGTH_SHORT).show();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            //Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
-                            //        Toast.LENGTH_SHORT).show();
-                            //updateUI(null);
-                            Toast.makeText(SignUpActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                        }
 
-                        // ...
+                        // Hide the progress bar
+                        mSignUpProgress.setVisibility(View.GONE);
+
+                        if (task.isSuccessful()) {
+                            // If the registration was successful, direct user to the MainActivity
+                            Intent i = MainActivity.newIntent(SignUpActivity.this);
+                            i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivityForResult(i, REQUEST_CODE_CALCULATE);
+                        } else {
+                            // Check if the user already exists
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                Toast.makeText(getApplicationContext(), "User already regisered", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
                     }
                 });
     }
