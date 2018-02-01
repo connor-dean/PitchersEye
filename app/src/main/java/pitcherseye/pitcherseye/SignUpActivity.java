4296263aeddffc22a5ca4339e6737dc8f4b29243
Connor Dean <connor.dean@washburn.edu>
2018-02-01 15:15:20 -0600

    Cleanup imports

--- app/src/main/java/pitcherseye/pitcherseye/SignUpActivity.java
+++ app/src/main/java/pitcherseye/pitcherseye/SignUpActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -18,9 +17,6 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseUser;
-
-import static java.security.AccessController.getContext;
 
 public class SignUpActivity extends AppCompatActivity {
 
