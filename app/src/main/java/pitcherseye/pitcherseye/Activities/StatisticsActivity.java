5b0ffb3f7cedf07ea81cda44b015234a369137fc
Jess Wilson <wess.jilson.11@gmail.com>
2018-03-13 09:28:20 -0500

    first successful firebase retrieve

--- app/src/main/java/pitcherseye/pitcherseye/Activities/StatisticsActivity.java
+++ app/src/main/java/pitcherseye/pitcherseye/Activities/StatisticsActivity.java
@@ -3,15 +3,13 @@ package pitcherseye.pitcherseye.Activities;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
-import android.view.View;
-import java.util.ArrayList;
-import java.util.Iterator;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -21,6 +19,10 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import pitcherseye.pitcherseye.Objects.EventStats;
 import pitcherseye.pitcherseye.R;
 
 //Need to do layout constraints for everything
@@ -29,135 +31,109 @@ public class StatisticsActivity extends AppCompatActivity {
     ArrayList<String> listKeys = new ArrayList<String>();
     ArrayAdapter<String> adapter;
 
-    private ListView dataListView;
-    private Boolean searchMode = false;
-    private Boolean itemSelected = false;
-    private int selectedPosition = 0;
-    private EditText itemText;
-    private Button findButton;
-    private Button deleteButton;
+    ListView listView;
+    Button findButton;
+    Button deleteButton;
+    EditText itemText;
+    Boolean searchMode = false;
+    Boolean itemSelected = false;
+    int selectedPosition = 0;
 
-    private FirebaseDatabase database = FirebaseDatabase.getInstance();
-    //Instance of the database reference configured with a path of "/todo"
-    private DatabaseReference dbRef = database.getReference("todo");
+
+    DatabaseReference dbref = FirebaseDatabase.getInstance().getReference().child("eventStats");
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-        dataListView = (ListView) findViewById(R.id.statistics_list_view);
-        itemText = (EditText) findViewById(R.id.itemText);
-        findButton = (Button) findViewById(R.id.findButton);
-        deleteButton = (Button) findViewById(R.id.deleteButton);
+
+        listView = (ListView)findViewById(R.id.statistics_list_view);
+        findButton = (Button)findViewById(R.id.findButton);
+        deleteButton = (Button)findViewById(R.id.deleteButton);
+        itemText = (EditText)findViewById(R.id.itemText);
+
+        itemText.setText("");
         deleteButton.setEnabled(false);
 
         adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,listItems);
-        dataListView.setAdapter(adapter);
-        dataListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-
-        dataListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                selectedPosition = position;
-                itemSelected = true;
-                deleteButton.setEnabled(true);
-            }
-        });
-        addChildEventListener();
-    }
-
-    public void addChildEventListener(){
-        ChildEventListener childListener = new ChildEventListener(){
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s){
-                adapter.add((String) dataSnapshot.child("eventStats").getValue());
-            }
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s){
-
-            }
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s){
-
-            }
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot){
-                String key = dataSnapshot.getKey();
-                int index = listKeys.indexOf(key);
-
-                if (index != -1){
-                    listItems.remove(index);
-                    listKeys.remove(index);
-                    adapter.notifyDataSetChanged();
-                }
-            }
-            @Override
-            public void onCancelled(DatabaseError databaseError){
-
-            }
-        };
-        dbRef.addChildEventListener(childListener);
-    }
-    //onChildAdded() will be called when a new child is added to the data list and will be passed
-    // a datasnapshot object containing the key and value of the new child
-    //onChildRemoved() will be called when a child is removed form the list.
-
-    public void deleteItem(View view){
-        dataListView.setItemChecked(selectedPosition, false);
-        dbRef.child(listKeys.get(selectedPosition)).removeValue();
-    }
-    //Deselects current selected item in ListView nad uses the selectedPosition variable as an index
-    //into the listKeys array. Having identified the key associated with the item to be deleted,
-    //that key is used to remove hte child node from the database
-
-    public void findItems(View view){
-        ValueEventListener queryValueListener = new ValueEventListener() {
+        listView.setAdapter(adapter);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        dbref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                Iterable<DataSnapshot> snapshotIterator = dataSnapshot.getChildren();
-                Iterator<DataSnapshot> iterator = snapshotIterator.iterator();
-
-                adapter.clear();
-                listKeys.clear();
-
-                while (iterator.hasNext()) {
-                    DataSnapshot next = (DataSnapshot) iterator.next();
-
-                    String match = (String) next.child("eventStats").getValue();
-                    String key = next.getKey();
-                    listKeys.add(key);
-                    adapter.add(match);
-                }
             }
-            //onDataChange() will be called when the query returns and will be passed a data snapshot
-            // containing the items that match the search criteria. Method clears both the adapter
-            // and listKeys arrays and then iterates through the children in the snapshot. For each
-            // child, the key is stored on the listKeys array and teh value added to the adapter so
-            // that it will appear in the ListView
 
             @Override
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        };
+        });
+        dbref.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                adapter.add((String) dataSnapshot.getKey());
+                listKeys.add(dataSnapshot.getKey());
+                //adapter.add((String) dataSnapshot.child("eventStats").getKey());
 
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String key = dataSnapshot.getKey();
+                int index = listKeys.indexOf(key);
+
+                if(index != -1){
+                    listItems.remove(index);
+                    listKeys.remove(index);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedPosition = i;
+                itemSelected = true;
+                deleteButton.setEnabled(true);
+            }
+        });
+
+    }
+
+    public void findItems(View view){
         Query query;
-
         if(!searchMode){
             findButton.setText("Clear");
-            query = dbRef.orderByChild("description").equalTo(itemText.getText().toString());
+            query = dbref.orderByChild("eventStats").equalTo(itemText.getText().toString());
             searchMode = true;
-        } else {
+        } else{
             searchMode = false;
             findButton.setText("Find");
-            query = dbRef.orderByKey();
+            query = dbref.orderByKey();
         }
         if(itemSelected){
-            dataListView.setItemChecked(selectedPosition, false);
+            listView.setItemChecked(selectedPosition, false);
             itemSelected = false;
             deleteButton.setEnabled(false);
         }
-        query.addListenerForSingleValueEvent(queryValueListener);
+        //query.addListenerForSingleValueEvent(queryValueListener);
     }
+
 
     public static Intent newIntent(Context packageContext) {
         Intent i = new Intent(packageContext, StatisticsActivity.class);
--- app/src/main/java/pitcherseye/pitcherseye/Objects/EventStats.java
+++ app/src/main/java/pitcherseye/pitcherseye/Objects/EventStats.java
@@ -57,4 +57,138 @@ public class EventStats {
         this.R3C2Count = R3C2Count;
         this.R3C3Count = R3C3Count;
     }
+    public String getEventID() {
+        return eventID;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public String getEventDate() {
+        return eventDate;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public int getTeamID() {
+        return teamID;
+    }
+
+    public int getPitchCount() {
+        return pitchCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getR1C1Count() {
+        return R1C1Count;
+    }
+
+    public int getR1C2Count() {
+        return R1C2Count;
+    }
+
+    public int getR1C3Count() {
+        return R1C3Count;
+    }
+
+    public int getR2C1Count() {
+        return R2C1Count;
+    }
+
+    public int getR2C2Count() {
+        return R2C2Count;
+    }
+
+    public int getR2C3Count() {
+        return R2C3Count;
+    }
+
+    public int getR3C1Count() {
+        return R3C1Count;
+    }
+
+    public int getR3C2Count() {
+        return R3C2Count;
+    }
+
+    public int getR3C3Count() {
+        return R3C3Count;
+    }
+    public void setEventID(String eventID) {
+        this.eventID = eventID;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public void setEventDate(String eventDate) {
+        this.eventDate = eventDate;
+    }
+
+    public void setPlayerID(int playerID) {
+        this.playerID = playerID;
+    }
+
+    public void setTeamID(int teamID) {
+        this.teamID = teamID;
+    }
+
+    public void setPitchCount(int pitchCount) {
+        this.pitchCount = pitchCount;
+    }
+
+    public void setStrikeCount(int strikeCount) {
+        this.strikeCount = strikeCount;
+    }
+
+    public void setBallCount(int ballCount) {
+        this.ballCount = ballCount;
+    }
+
+    public void setR1C1Count(int r1C1Count) {
+        R1C1Count = r1C1Count;
+    }
+
+    public void setR1C2Count(int r1C2Count) {
+        R1C2Count = r1C2Count;
+    }
+
+    public void setR1C3Count(int r1C3Count) {
+        R1C3Count = r1C3Count;
+    }
+
+    public void setR2C1Count(int r2C1Count) {
+        R2C1Count = r2C1Count;
+    }
+
+    public void setR2C2Count(int r2C2Count) {
+        R2C2Count = r2C2Count;
+    }
+
+    public void setR2C3Count(int r2C3Count) {
+        R2C3Count = r2C3Count;
+    }
+
+    public void setR3C1Count(int r3C1Count) {
+        R3C1Count = r3C1Count;
+    }
+
+    public void setR3C2Count(int r3C2Count) {
+        R3C2Count = r3C2Count;
+    }
+
+    public void setR3C3Count(int r3C3Count) {
+        R3C3Count = r3C3Count;
+    }
 }
