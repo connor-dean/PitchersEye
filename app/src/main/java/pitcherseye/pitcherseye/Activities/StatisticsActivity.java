bcb061f45d855503a877309d4c3b4f7342d80fc1
Jess Wilson <wess.jilson.11@gmail.com>
2018-04-03 11:03:26 -0500

    RecyclerViewAdapter 1

--- app/src/main/java/pitcherseye/pitcherseye/Activities/StatisticsActivity.java
+++ app/src/main/java/pitcherseye/pitcherseye/Activities/StatisticsActivity.java
@@ -1,21 +1,30 @@
 package pitcherseye.pitcherseye.Activities;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Image;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
@@ -23,120 +32,77 @@ import java.util.ArrayList;
 import java.util.List;
 
 import pitcherseye.pitcherseye.Objects.EventStats;
+import pitcherseye.pitcherseye.Objects.RecyclerViewAdapter;
 import pitcherseye.pitcherseye.R;
 
+//https://www.androidhive.info/2016/01/android-working-with-recycler-view/
+//https://stackoverflow.com/questions/40584424/simple-android-recyclerview-example
+//https://firebaseui.com/docs/android/index.html?com/firebase/ui/FirebaseRecyclerViewAdapter.html\
+//https://www.coderefer.com/firebaseui-android-firebase-database/
+//https://www.coderefer.com/android-recyclerview-cardview-tutorial/
 //Need to do layout constraints for everything
 public class StatisticsActivity extends AppCompatActivity {
-    ArrayList<String> listItems = new ArrayList<String>();
-    ArrayList<String> listKeys = new ArrayList<String>();
-    ArrayAdapter<String> adapter;
+    //DatabaseReference databaseReference; // = FirebaseDatabase.getInstance().getReference().child("eventStats");
+    private RecyclerView recyclerView;
+    private DatabaseReference myref;
 
-    ListView listView;
-    Button findButton;
-    Button deleteButton;
-    EditText itemText;
-    Boolean searchMode = false;
-    Boolean itemSelected = false;
-    int selectedPosition = 0;
-
-
-    DatabaseReference dbref = FirebaseDatabase.getInstance().getReference().child("eventStats");
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        listView = (ListView)findViewById(R.id.statistics_list_view);
-        findButton = (Button)findViewById(R.id.findButton);
-        deleteButton = (Button)findViewById(R.id.deleteButton);
-        itemText = (EditText)findViewById(R.id.itemText);
-
-        itemText.setText("");
-        deleteButton.setEnabled(false);
-
-        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_single_choice,listItems);
-        listView.setAdapter(adapter);
-        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        dbref.addValueEventListener(new ValueEventListener() {
+        myref = FirebaseDatabase.getInstance().getReference("https://pitcherseye-3cdd0.firebaseapp.com/");
+        FirebaseRecyclerViewAdapter<EventStats, EventStatsViewHolder> adapter;
+        adapter = new FirebaseRecyclerViewAdapter<EventStats, EventStatsViewHolder>(EventStats.class, R.layout.individual_row, EventStatsViewHolder.class, myref){
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void populateViewHolder(EvenStatsViewHolder eventStatsViewHolder, EventStats eventStats){
+
+                        eventStatsViewHolder.nameText.setText(eventStats.getEventName());
+                        eventStatsViewHolder.messageText.setText(eventStats.getEventID());
             }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-        dbref.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                adapter.add((String) dataSnapshot.getKey());
-                listKeys.add(dataSnapshot.getKey());
-                //adapter.add((String) dataSnapshot.child("eventStats").getKey());
+        };
+        /*FirebaseRecyclerAdapter<EventStats, EventStatsViewHolder>
+            recyclerAdapter = new FirebaseRecyclerAdapter<EventStats, EventStatsViewHolder>(EventStats.class, R.layout.individual_row,EventStatsViewHolder.class, myref){
+                    @Override
+                    protected void populateViewHolder(EventStatsViewHolder viewHolder, EventStats model,int position){
+                    viewHolder.setTitle(model.getEventName());
+                    viewHolder.setDescription(model.getEventID());
 
             }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                String key = dataSnapshot.getKey();
-                int index = listKeys.indexOf(key);
-
-                if(index != -1){
-                    listItems.remove(index);
-                    listKeys.remove(index);
-                    adapter.notifyDataSetChanged();
-                }
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                selectedPosition = i;
-                itemSelected = true;
-                deleteButton.setEnabled(true);
-            }
-        });
+        };*/
+        //recyclerView.setAdapter(adapter);
 
     }
-
-    public void findItems(View view){
-        Query query;
-        if(!searchMode){
-            findButton.setText("Clear");
-            query = dbref.orderByChild("eventStats").equalTo(itemText.getText().toString());
-            searchMode = true;
-        } else{
-            searchMode = false;
-            findButton.setText("Find");
-            query = dbref.orderByKey();
-        }
-        if(itemSelected){
-            listView.setItemChecked(selectedPosition, false);
-            itemSelected = false;
-            deleteButton.setEnabled(false);
-        }
-        //query.addListenerForSingleValueEvent(queryValueListener);
-    }
-
 
     public static Intent newIntent(Context packageContext) {
         Intent i = new Intent(packageContext, StatisticsActivity.class);
         return i;
+    }
+    public static class EventStatsViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        TextView textView_title;
+        TextView textView_description;
+        TextView textView_otherdescription;
+        ImageView imageView;
+        public EventStatsViewHolder(View itemView){
+            super(itemView);
+
+            mView = itemView;
+            textView_title = (TextView)itemView.findViewById(R.id.title);
+            textView_description = (TextView)itemView.findViewById(R.id.description);
+            textView_otherdescription = (TextView)itemView.findViewById(R.id.otherdescription);
+            imageView = (ImageView)itemView.findViewById(R.id.image);
+        }
+
+        public void setTitle(String title){
+            textView_title.setText(title+"");
+        }
+        public void setDescription(String description){
+            textView_description.setText(description);
+        }
     }
 }
