7e713a599290cf1b323be0fb84fc795b9429c43a
connor-dean <connor.dean@washburn.edu>
2018-11-17 14:17:11 -0600

    Butterknife'd ChangePitcherFragment

--- app/src/main/java/pitcherseye/pitcherseye/Activities/MainActivity.java
+++ app/src/main/java/pitcherseye/pitcherseye/Activities/MainActivity.java
@@ -10,7 +10,6 @@
 
 package pitcherseye.pitcherseye.Activities;
 
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
